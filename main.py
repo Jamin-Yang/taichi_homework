551c5c349741de539e2b6006936ffc1b38a9c483
Jamin-Yang <91379790+Jamin-Yang@users.noreply.github.com>
2022-01-06 19:52:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,42 @@ import taichi as ti
 from color import getPixel
 ti.init(arch=ti.gpu)
 
+output = False                       # false for output as gif, true for gui
 resx = 1024
 resy = 512
 color = ti.Vector.field(3, ti.f32, shape=(resx,resy))
 
-gui = ti.GUI('Waves', (resx, resy))
 
 @ti.kernel
 def draw(t:ti.f32):
      for i,j in color:
         color[i, j] = getPixel(i, j, t)
+     
+if output:
+    gui = ti.GUI('sea', res=(resx,resy))
+else:
+    result_dir = "./finalresults"
+    video_manager = ti.VideoManager(output_dir=result_dir, framerate=24, automatic_build=True)
 
 
-for i in range(10000):
-    t = i * 0.003
-    draw(t)
-    
-    gui.set_image(color.to_numpy())
-    gui.show()
 
+if output:
+    for i in range(100):
+        t = i * 0.005
+        draw(t)
+        pixels_img = color.to_numpy()
+        gui.set_image(pixels_img)
+        gui.show()
+else:
+    for i in range(100):
+        t = i * 0.005
+        draw(t)
+        pixels_img = color.to_numpy()
+        video_manager.write_frame(pixels_img)
+        print(f'\rFrame {i+1} is recorded', end='')
+
+if not output:
+    print()
+    print('Exporting .gif videos...')
+    video_manager.make_video(gif=True, mp4=False)
+    print(f'GIF video is saved to {video_manager.get_output_filename(".gif")}')
